b8c1d6210b6eb1abab9df28f2b6b7c1d1362621e
Carly Luo <carlythelaw@gmail.com>
2022-05-31 12:16:12 +0100

    add seed folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  user = User.create(
+    username: Faker::Internet.username,
+    password: Faker::Internet.password
+  )
+  Pokemon.create(
+    name: Faker::Games::Pokemon.name,
+    move: Faker::Games::Pokemon.move,
+    location: %w[Westminster Southwark Lambeth Hackney Greenwich Bromley Islington Camden].sample,
+    element: %w[Normal Fire Electric Rock Water].sample,
+    user: user
+  )
+end
