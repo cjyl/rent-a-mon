8a2cac2be7841a2df140b4e6edc3ea78f0f3af7c
Carly Luo <carlythelaw@gmail.com>
2022-06-01 11:48:25 +0100

    Started some work on api.

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,6 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
 end
