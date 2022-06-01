ede55cf357a366acc12f046d96616671cdee0b0c
Johan van Meeuwen <vanmeeuwen.j@gmail.com>
2022-06-01 14:09:33 +0100

    Added image logic to index and show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Pokemon.destroy_all
 User.destroy_all
 puts "Creating pokemon"
 
-24.times do
+15.times do
   user = User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
